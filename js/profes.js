cc91c61b59c882cb88405a03f9492397b141d56c
JP <javiera.parra.moya@ug.uchile.cl>
2025-06-03 15:18:29 -0400

    Update profes.js

--- js/profes.js
+++ js/profes.js
@@ -508,7 +508,7 @@ document.querySelectorAll("select")[0].addEventListener("change", (event) => {
     var notas = [];
     var notasPrevias = [];
     datos(event.target.value).catch((error) => console.error(error));
-    if(event.target.value == "Vielma Laguna, Mitzi"){
+    if(event.target.value == "Zorzano Betancourt, Osvaldo"){
         document.querySelector("#primera").classList.add("esconde");
         document.querySelector("#segunda").classList.remove("esconde")
     } else {
